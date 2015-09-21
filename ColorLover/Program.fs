5c1cb89112af6cd7488210b69a73852657e159ca
Jimmy D. Burrell <jburrellskype@hostsignalz.com>
2015-09-21 05:58:51 -0500

    got a json and xml preview working

---
+++ ColorLover/Program.fs
@@ -0,0 +1,15 @@
+﻿open FSharp.Data
+
+//type Palette = XmlProvider<"http://www.colourlovers.com/api/palettes">
+//let myPalette = Palette.GetSample()
+//printfn "%A \n" (myPalette)
+//for item in myPalette.Palettes do
+//  printfn " - %s (%s)" (item.Id.ToString()) (item.BadgeUrl.ToString())
+
+let apiUrlForPalettes = "http://www.colourlovers.com/api/palettes?format=json"
+type Palette = JsonProvider<"http://www.colourlovers.com/api/palette/92095?format=json">
+
+let myPalette = Palette.Load(apiUrlForPalettes)
+printfn "%A\n%A\n%A " myPalette.[0].BadgeUrl myPalette.[0].Url myPalette.[0].Title
+
+let pause = System.Console.ReadKey()
