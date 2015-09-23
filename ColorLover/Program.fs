2617e3f2ed1b703f767d52868340f63951fc3d18
Jimmy D. Burrell <jburrellskype@hostsignalz.com>
2015-09-23 16:50:38 -0500

    cleaned up the code and built a release

--- ColorLover/Program.fs
+++ ColorLover/Program.fs
@@ -1,14 +1,11 @@
 ﻿open FSharp.Data
 open FSharp.Data.HttpRequestHeaders
 
-//type Palette = JsonProvider<"http://www.colourlovers.com/api/palettes/random?format=json">
 type Palette = JsonProvider<"http://www.colourlovers.com/api/palette/92095?format=json">
-//let apiUrlForPalettes = "http://www.colourlovers.com/api/palette/92095?format=json"
 let apiUrlForPalettes = "http://www.colourlovers.com/api/palettes/random?format=json"
 
 let myPalette = Palette.Load(apiUrlForPalettes)
-//printfn "%A\n%A\n%A\n\n" myPalette.[0].BadgeUrl myPalette.[0].Url myPalette.[0].Title
-printfn "%A\n\n" myPalette.[0]
+//printfn "%A\n\n" myPalette.[0]
 
 let myTextRequest = "{
     \"username\" : \"" + myPalette.[0].Title + "\",
@@ -22,5 +19,3 @@ Http.RequestString(
     headers = [ ContentType HttpContentTypes.Json ],
     body = TextRequest myTextRequest
 ) |> printfn "%A"
-    
-let pause = System.Console.ReadKey()
