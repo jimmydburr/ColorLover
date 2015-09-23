34c3bdee1f7b05ad310cf697e2d1209dea45b4ad
Jimmy D. Burrell <jburrellskype@hostsignalz.com>
2015-09-23 16:33:33 -0500

    got everything working using a random palette

--- ColorLover/Program.fs
+++ ColorLover/Program.fs
@@ -1,17 +1,24 @@
 ﻿open FSharp.Data
 open FSharp.Data.HttpRequestHeaders
 
-let apiUrlForPalettes = "http://www.colourlovers.com/api/palettes?format=json"
+//type Palette = JsonProvider<"http://www.colourlovers.com/api/palettes/random?format=json">
 type Palette = JsonProvider<"http://www.colourlovers.com/api/palette/92095?format=json">
+//let apiUrlForPalettes = "http://www.colourlovers.com/api/palette/92095?format=json"
+let apiUrlForPalettes = "http://www.colourlovers.com/api/palettes/random?format=json"
 
 let myPalette = Palette.Load(apiUrlForPalettes)
 //printfn "%A\n%A\n%A\n\n" myPalette.[0].BadgeUrl myPalette.[0].Url myPalette.[0].Title
 printfn "%A\n\n" myPalette.[0]
-let myTextRequest = "{ \"URL\" : \"<" + myPalette.[0].Url + ">\" }"
-printfn "%s" myTextRequest
 
+let myTextRequest = "{
+    \"username\" : \"" + myPalette.[0].Title + "\",
+    \"icon_url\" : \"" + myPalette.[0].ImageUrl + "\",
+    \"text\" : \"<" + myPalette.[0].Url + ">\"
+}"
+printfn "%s\n" myTextRequest
+// hightower slack #creative channel
 Http.RequestString(
-    "http://httpbin.org/post", 
+    "https://hooks.slack.com/services/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",
     headers = [ ContentType HttpContentTypes.Json ],
     body = TextRequest myTextRequest
 ) |> printfn "%A"
