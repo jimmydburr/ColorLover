30e19da2867dcc787d0b7ad74133d6b6cc1fce99
Jimmy D. Burrell <jburrellskype@hostsignalz.com>
2015-09-23 11:48:37 -0500

    put body of JSON request into a variable

--- ColorLover/Program.fs
+++ ColorLover/Program.fs
@@ -7,11 +7,13 @@ type Palette = JsonProvider<"http://www.colourlovers.com/api/palette/92095?forma
 let myPalette = Palette.Load(apiUrlForPalettes)
 //printfn "%A\n%A\n%A\n\n" myPalette.[0].BadgeUrl myPalette.[0].Url myPalette.[0].Title
 printfn "%A\n\n" myPalette.[0]
+let myTextRequest = "{ \"URL\" : \"<" + myPalette.[0].Url + ">\" }"
+printfn "%s" myTextRequest
 
 Http.RequestString(
     "http://httpbin.org/post", 
     headers = [ ContentType HttpContentTypes.Json ],
-    body = TextRequest """ {"test": 42} """
+    body = TextRequest myTextRequest
 ) |> printfn "%A"
     
 let pause = System.Console.ReadKey()
